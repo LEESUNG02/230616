84f5f4bf63e2feea7430d398e429105c9a9201b0
LEESUNG02 <leesung02@naver.com>
2023-06-16 12:41:05 +0900

    이미지2

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ st.write("""
          2. 있는
          4. 리스트 - 숫자를 건너 뛰어도 무시하고 순서를 따름
             1. 들여쓰기1
-                2. 들여쓰기2 #1로 시작하지 않으면 들여쓰기는
+                1. 들여쓰기2 #1로 시작하지 않으면 들여쓰기는
                     1. 들여쓰기3
         1. 순서가
         1. 1로 넣어도
@@ -130,6 +130,9 @@ st.write(f"""
     * ![이미지에 대한 설명](https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA1MjNfMjYx%2FMDAxNjg0ODQyOTU2MzAy.NqVTY2XhBU4Tv_TNXQ9BB_Mm9TNadkXibZZ_XpDYuxAg.rvcI1B9yOW2yFvekAvsjFKfttnvYw2B_xRCV4P5Mg4sg.PNG.jisoo-060202%2Fimage.png&type=sc960_832)
     * ![이미지에 대한 설명]({l2})
     * [![이미지에 대한 설명](https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA1MjNfMjYx%2FMDAxNjg0ODQyOTU2MzAy.NqVTY2XhBU4Tv_TNXQ9BB_Mm9TNadkXibZZ_XpDYuxAg.rvcI1B9yOW2yFvekAvsjFKfttnvYw2B_xRCV4P5Mg4sg.PNG.jisoo-060202%2Fimage.png&type=sc960_832)
+    
+    
+    
 """)
 
 #인용
@@ -186,4 +189,73 @@ with col1:
 # 블록 (:) 을 열면 -> 이 안에서는 streamlit 기능 실행시 col2에 종속
 with col2:
     st.write("오른쪽")
+
+# tabs = st.tabs(["김치찌ㅉ개", "된장찌개", "로제마라어묵찌개"])
+tab_meins = ["김치찌ㅉ개", "된장찌개", "로제마라어묵찌개"]
+tab1, tab2, tab3 = st.tabs(tab_meuns)
+img1 = "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA0MTJfMTg1%2FMDAxNjgxMjk1NzY4MTA3.byuBE3llW2ljkxpopgBTZRrbxZ-tz4SZNa20DmY0Ylsg.MqfIeGQZvtz25GXK1eBDeQtD5ZZxjwiPWIawS6pdr2gg.JPEG.iamjieunpark%2F1681295600930.jpg&type=sc960_832"
+tab1.image(img1)
+whit tab2:
+    img2 ="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA1MThfMjEx%2FMDAxNjg0MzY5NDYzMTU1.w1M2SqkJHLAGvGaj-fb3VGLtjHpdgpTqDFWrRxaOB-sg.iNA0Z1LzkUvKyeBH1HHdKTS9cUp5HnJcOSzpv5Qch68g.JPEG.cera23%2FIMG_3947.JPG&type=sc960_832"
+    st.image(img2)
+tab3.write("이런건 없어요...상상도 마라요")
+
+exp = st.expander("Surprise!!!", expanded=false)
+exp.image("")
+# whit exp: ...
+
+# 입력
+st.title("입력")
+# st.text_input("나의 이름은")
+name = st.text_input("나의 이름은") # 변수로 받을수있음
+name2 = st.text_input("너의 이름은") # 변수로 받을수있음
+# st.text_input("")  -> 이것도 가능
+# st.write(name)
+# st.write(name2)
+st.write(f"신랑 {name}과 신부{name2}는 ...")
+# number = st.number_input("당신의 나이는?")
+number = st.number_input("당신의 나이는?", step=1) # step는 하나의
+# st.write(f"나의 나이는 {number}세")
+st.write(f"나의 나이는 {age}세")
+height = st.number_input("당신의 키는?", step=10)
+st.write(f"나의 키는 {height}cm")
+# https://docs.streamlit.io/library/api-reference/widgets
+
+
+st.divider()
+mode = st.checkbox('강사님 잔소리모드')  # bool (T/F)
+r = st.radio("잔소리 내용 선택", ["취업", "지각", "코딩"])
+if mode:
+    # r -> 취업, 코딩, 지각
+    if r == "취업":
+        for i in range(s):
+            st.write("여러분 8월에는 자소서 넣어야겠죠?")
+    else r == "코딩":
+        st.write("저보다 파이썬 잘해요?")
+    else r == "지각":
+        st.write("9시랑 9시 1분은 다른 거예요")
+
+st.divider()
+mode = st.checkbox('강사님 잔소리모드')  # bool (T/F)
+col1, col2, col3 = st.columns(3)
+r = col2.radio("잔소리 내용 선택", ["취업", "지각", "코딩"])
+s = col2.slider("잔소리 강도 선택", min_value=1, max_value=10)    # min_value, max_value 목소리 낮추는 옵션
+b = col3.selectbox("잔소리 말투 선택",["친절하게", "반말", "모욕적"])
+if mode:
+    # r -> 취업, 코딩, 지각
+    format = Nome
+    if b == "친절하게"
+        format = lambda x: f"여러분~ {x}"
+    else: b == "반말"
+        format = lambda x: f"야! {x}"
+    else b == "모욕적"
+        format - lambda x: f"xxxxx! {x}"
+    if r == "취업":
+        for i in range(s):
+            st.write(format("여러분 8월에는 자소서 넣어야겠죠?"))
+    else r == "코딩":
+        st.write(format("저보다 파이썬 잘해요?"))
+    else r == "지각":
+        st.write(format("9시랑 9시 1분은 다른 거예요"))
+
 
--- pages/컴포넌트.py
+++ pages/컴포넌트.py
@@ -1,4 +1,30 @@
 import streamlit as st
 
+st.title("컴포넌트")
+# 위-아래로 한줄로만...
 st.write("🍉")
+cols = st.columns(2) # 컬럼 리스트
+cols[0].write("🍉")
+cols[1].write("🍉")
+
+cols = st.columns(3)
+# 🌋 -> n 등분 -> 3등분
+cols[0].write("🌋")
+cols[1].write("🌋")
+cols[-1].write("🌋")
+cols = cols[0].columns(3) # 열의 열린 거임
+cols[0].write("🌋")
+cols[1].write("🌋")
+cols[-1].write("🌋")
+col1, col2 = st.columns(2) # 리스트 언패킹
+col1.write("왼쪽 열")
+col2.write("오른쪽 열")
+
+with col1:
+    st.write("왼쪽")
+# col1을 기준으로 streamlit을 써주겠다
+# 블록 (:) 을 열면 -> 이 안에서는 streamlit 기능 실행시 col2에 종속
+with col2:
+    st.write("오른쪽")
+
 
